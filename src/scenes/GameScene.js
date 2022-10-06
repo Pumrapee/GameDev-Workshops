88107cf1bacbda122aac98883ca10c9ffae5e593
panithee <panithee.pnt@mail.kmutt.ac.th>
2022-10-06 21:27:04 +0700

    add template

---
+++ src/scenes/GameScene.js
@@ -0,0 +1,23 @@
+import Phaser from "phaser";
+
+class GameScene extends Phaser.Scene {
+    constructor(test) {
+        super({
+            key: "GameScene",
+        });
+    }
+
+    preload() {
+        // code here
+    }
+
+    create() {
+        // code here
+    }
+
+    update(delta, time) {
+        // code here
+    }
+}
+export default GameScene;
+
